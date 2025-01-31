dbc93a2ba7d364e4d1f40def3a85234802331f8c
Nenad Vujicic <nenadus@gmail.com>
2025-01-31 19:00:11 +0100

    Adds OldNote model file
    Adds OldNote model file for modeling one row of old_notes table. Also, added author association for connecting with users table.

---
+++ app/models/old_note.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: old_notes
+#
+#  note_id     :bigint           not null, primary key
+#  latitude    :integer          not null
+#  longitude   :integer          not null
+#  tile        :bigint           not null
+#  timestamp   :datetime         not null
+#  status      :enum             not null
+#  description :text             default(""), not null
+#  user_id     :bigint
+#  user_ip     :inet
+#  version     :bigint           default(1), not null, primary key
+#
+# Indexes
+#
+#  index_old_notes_on_version  (version)
+#
+# Foreign Keys
+#
+#  old_notes_note_id_fkey  (note_id => notes.id)
+#  old_notes_user_id_fkey  (user_id => users.id)
+#
+
+class OldNote < ApplicationRecord
+  belongs_to :author, :class_name => "User", :foreign_key => "user_id", :optional => true, :inverse_of => :old_notes
+end
